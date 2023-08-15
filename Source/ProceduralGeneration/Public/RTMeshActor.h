aa84214618a1478754ad6fa451f32f065dac2b0e
Fazer4900 <67338328+Fazer4900@users.noreply.github.com>
2023-08-15 20:08:47 +0200

    Initial commit

---
+++ Source/ProceduralGeneration/Public/RTMeshActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RealtimeMeshActor.h"
+#include "RTMeshActor.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALGENERATION_API ARTMeshActor : public ARealtimeMeshActor
+{
+	GENERATED_BODY()
+
+	int penis = 6;
+	
+};
